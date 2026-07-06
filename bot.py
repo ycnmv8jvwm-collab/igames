253df630619d92aabb49181ddab5816708867be5
ycnmv8jvwm-collab <ycnmv8jvwm@privaterelay.appleid.com>
2026-07-06 17:17:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiohttp import web
 
 from config import BOT_TOKEN
 from handlers import main_router
+from api import create_app
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,6 +15,14 @@ async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(main_router)
+
+    # Запускаем API сервер и бота одновременно
+    api_app = create_app()
+    runner = web.AppRunner(api_app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
+    await site.start()
+    logging.info("API server started on port 8080")
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
